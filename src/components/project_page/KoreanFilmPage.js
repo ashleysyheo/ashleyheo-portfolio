f4ed07cea5a08bff4572f046f0e8685e01f11a58
ashleysheo <ashleysyheo@gmail.com>
2020-12-16 00:22:12 +0900

    fixed image stretching

--- src/components/project_page/KoreanFilmPage.js
+++ src/components/project_page/KoreanFilmPage.js
@@ -24,37 +24,37 @@ const KoreanFilmPage = () => {
                 </div>
 
                 <div className='image-container'>
-                    <div className='heading2'>Worst Woman</div>
+                    <div className='heading2 inline-block' >Worst Woman</div>
                     <ProgressiveImage src='https://ashleyheo-portfolio.s3.ap-northeast-2.amazonaws.com/assets/korean-film/korean-film1.png' placeholder='https://ashleyheo-portfolio.s3.ap-northeast-2.amazonaws.com/assets/korean-film/korean-film1-small.png'>
-                        {(src, loading) => <img className='img--small-2 margin-bottom--big' style={{ filter: loading ? "blur(10px)" : 'none', transition: loading ? "none" : "filter 0.2s ease-out" }} src={src} alt='Korean Film Poster: Worst Woman' />}
+                        {(src, loading) => <img className='img--small-2 margin-bottom--big' style={{ filter: loading ? "blur(10px)" : 'none', transition: loading ? "none" : "filter 0.2s ease-out", alignSelf: "center" }} src={src} alt='Korean Film Poster: Worst Woman' />}
                     </ProgressiveImage>
                 </div>
 
                 <div className='image-container'>
                     <div className='heading2'>Little Forest</div>
                     <ProgressiveImage src='https://ashleyheo-portfolio.s3.ap-northeast-2.amazonaws.com/assets/korean-film/korean-film2.png' placeholder='https://ashleyheo-portfolio.s3.ap-northeast-2.amazonaws.com/assets/korean-film/korean-film2-small.png'>
-                        {(src, loading) => <img className='img--small-2 margin-bottom--big' style={{ filter: loading ? "blur(10px)" : 'none', transition: loading ? "none" : "filter 0.2s ease-out" }} src={src} alt='Korean Film Poster: Little Forest' />}
+                        {(src, loading) => <img className='img--small-2 margin-bottom--big' style={{ filter: loading ? "blur(10px)" : 'none', transition: loading ? "none" : "filter 0.2s ease-out", alignSelf: "center" }} src={src} alt='Korean Film Poster: Little Forest' />}
                     </ProgressiveImage>
                 </div>
 
                 <div className='image-container'>
                     <div className='heading2'>Bleak Night</div>
                     <ProgressiveImage src='https://ashleyheo-portfolio.s3.ap-northeast-2.amazonaws.com/assets/korean-film/korean-film3.png' placeholder='https://ashleyheo-portfolio.s3.ap-northeast-2.amazonaws.com/assets/korean-film/korean-film3-small.png'>
-                        {(src, loading) => <img className='img--small-2 margin-bottom--big' style={{ filter: loading ? "blur(10px)" : 'none', transition: loading ? "none" : "filter 0.2s ease-out" }} src={src} alt='Korean Film Poster: Little Forest' />}
+                        {(src, loading) => <img className='img--small-2 margin-bottom--big' style={{ filter: loading ? "blur(10px)" : 'none', transition: loading ? "none" : "filter 0.2s ease-out", alignSelf: "center" }} src={src} alt='Korean Film Poster: Little Forest' />}
                     </ProgressiveImage>
                 </div>
 
                 <div className='image-container'>
                     <div className='heading2'>King of Jokgu</div>
                     <ProgressiveImage src='https://ashleyheo-portfolio.s3.ap-northeast-2.amazonaws.com/assets/korean-film/korean-film4.png' placeholder='https://ashleyheo-portfolio.s3.ap-northeast-2.amazonaws.com/assets/korean-film/korean-film4-small.png'>
-                        {(src, loading) => <img className='img--small-2 margin-bottom--big' style={{ filter: loading ? "blur(10px)" : 'none', transition: loading ? "none" : "filter 0.2s ease-out" }} src={src} alt='Korean Film Poster: Little Forest' />}
+                        {(src, loading) => <img className='img--small-2 margin-bottom--big' style={{ filter: loading ? "blur(10px)" : 'none', transition: loading ? "none" : "filter 0.2s ease-out", alignSelf: "center" }} src={src} alt='Korean Film Poster: Little Forest' />}
                     </ProgressiveImage>
                 </div>
 
                 <div className='image-container'>
                     <div className='heading2'>Social Phobia</div>
                     <ProgressiveImage src='https://ashleyheo-portfolio.s3.ap-northeast-2.amazonaws.com/assets/korean-film/korean-film5.png' placeholder='https://ashleyheo-portfolio.s3.ap-northeast-2.amazonaws.com/assets/korean-film/korean-film5-small.png'>
-                        {(src, loading) => <img className='img--small-2 margin-bottom--big' style={{ filter: loading ? "blur(10px)" : 'none', transition: loading ? "none" : "filter 0.2s ease-out" }} src={src} alt='Korean Film Poster: Little Forest' />}
+                        {(src, loading) => <img className='img--small-2 margin-bottom--big' style={{ filter: loading ? "blur(10px)" : 'none', transition: loading ? "none" : "filter 0.2s ease-out", alignSelf: "center" }} src={src} alt='Korean Film Poster: Little Forest' />}
                     </ProgressiveImage>
                 </div>
 
